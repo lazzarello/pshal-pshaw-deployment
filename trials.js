bfa4cd8a53b136dbc7d9ea4fc8973314c7469e07
Lee Azzarello <lee@rockingtiger.com>
2024-12-29 16:31:17 -0800

    add trials

---
+++ trials.js
@@ -0,0 +1,727 @@
+export const stimuliList = [
+  {
+    "type": "text",
+    "file": "",
+    "text": "This is an intro session||Read, Listen, and repeat | the following series of words | Please say each word only once  "
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Town"
+  },
+    {
+        "type": "audio",
+        "file": "LIKE.mp3",
+        "text": ""
+    },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Bite"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Blame"
+  },
+  {
+    "type": "audio",
+    "file": "MAY.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "POUND.mp3",
+    "text": ""
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Where"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Void"
+  },
+  {
+    "type": "text",
+    "file": "",
+    "text": "Thank you that was a test session | Please wait for further instructions"
+  },
+  {
+    "type": "text",
+    "file": "",
+    "text": "Read and repeat the following series of words | Please say each word only once  "
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Town"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Brown"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Frown"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Mouth"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Down"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Pound"
+  },
+  {
+    "type": "text",
+    "file": "",
+    "text": "Listen and repeat the following series of words | Please say each word only once  "
+  },
+  {
+    "type": "audio",
+    "file": "TOWN.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "BROWN.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "FROWN.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "MOUTH.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "DOWN.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "POUND.mp3",
+    "text": ""
+  },
+  {
+    "type": "text",
+    "file": "",
+    "text": "Read and repeat the following series of words | Please say each word only once  "
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Light"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Bite"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Crime"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Might"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Pride"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Bike"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Cry"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Why"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Like"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Slide"
+  },
+  {
+    "type": "text",
+    "file": "",
+    "text": "Listen and repeat the following series of words | Please say each word only once  "
+  },
+  {
+    "type": "audio",
+    "file": "LIGHT.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "BITE.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "CRIME.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "MIGHT.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "PRIDE.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "BIKE.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "CRY.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "WHY.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "LIKE.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "SLIDE.mp3",
+    "text": ""
+  },
+  {
+    "type": "text",
+    "file": "",
+    "text": "Read and repeat the following series of words | Please say each word only once  "
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Play"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Plate"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Blame"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "May"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Wait"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Freight"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "They"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Great"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Space"
+  },
+  {
+    "type": "text",
+    "file": "",
+    "text": "Listen and repeat the following series of words | Please say each word only once  "
+  },
+  {
+    "type": "audio",
+    "file": "PLAY.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "PLATE.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "BLAME.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "MAY.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "WAIT.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "FREIGHT.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "THEY.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "GREAT.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "SPACE.mp3",
+    "text": ""
+  },
+  {
+    "type": "text",
+    "file": "",
+    "text": "Read and repeat the following series of words | Please say each word only once  "
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Pair"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Fair"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Hair"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Mare"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Where"
+  },
+  {
+    "type": "text",
+    "file": "",
+    "text": "Listen and repeat the following series of words | Please say each word only once  "
+  },
+  {
+    "type": "audio",
+    "file": "PAIR.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "FAIR.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "HAIR.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "MARE.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "WHERE.mp3",
+    "text": ""
+  },
+  {
+    "type": "text",
+    "file": "",
+    "text": "Read and repeat the following series of words | Please say each word only once  "
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Deer"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Peel"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Dear"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Meal"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Atmosphere"
+  },
+  {
+    "type": "text",
+    "file": "",
+    "text": "Listen and repeat the following series of words | Please say each word only once  "
+  },
+  {
+    "type": "audio",
+    "file": "DEER.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "PEEL.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "DEAR.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "MEAL.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "ATMOSPHERE.mp3",
+    "text": ""
+  },
+  {
+    "type": "text",
+    "file": "",
+    "text": "Read and repeat the following series of words | Please say each word only once  "
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Slow"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Know"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Boat"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Rose"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Poke"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Row"
+  },
+  {
+    "type": "text",
+    "file": "",
+    "text": "Listen and repeat the following series of words | Please say each word only once  "
+  },
+  {
+    "type": "audio",
+    "file": "SLOW.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "KNOW.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "BOAT.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "ROSE.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "POKE.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "ROW.mp3",
+    "text": ""
+  },
+  {
+    "type": "text",
+    "file": "",
+    "text": "Read and repeat the following series of words | Please say each word only once  "
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Toy"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Boy"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Void"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Employ"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Noise"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Coin"
+  },
+  {
+    "type": "text",
+    "file": "",
+    "text": "Listen and repeat the following series of words | Please say each word only once  "
+  },
+  {
+    "type": "audio",
+    "file": "TOY.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "BOY.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "VOID.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "EMPLOY.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "NOISE.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "COIN.mp3",
+    "text": ""
+  },
+  {
+    "type": "text",
+    "file": "",
+    "text": "Read and repeat the following series of words | Please say each word only once  "
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Sure"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Endure"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Curious"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Jury"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Pure"
+  },
+  {
+    "type": "text",
+    "file": "",
+    "text": "Listen and repeat the following series of words | Please say each word only once  "
+  },
+  {
+    "type": "audio",
+    "file": "SURE.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "ENDURE.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "CURIOUS.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "JURY.mp3",
+    "text": ""
+  },
+  {
+    "type": "audio",
+    "file": "PURE.mp3",
+    "text": ""
+  },
+  {
+    "type": "text",
+    "file": "",
+    "text": "Read and repeat the following series of words | Please say each word only once  "
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Bit"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Hit"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Bet"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Bat"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "But"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Put"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Boot"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Bought"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Boat"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Hot"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "About"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Her"
+  },
+  {
+    "type": "word",
+    "file": "",
+    "text": "Bird"
+  },
+  {
+    "type": "text",
+    "file": "",
+    "text": "This session is now complete| Thank you"
+  }
+]
